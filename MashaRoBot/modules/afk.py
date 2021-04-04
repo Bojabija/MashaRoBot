884e4cbe7056c8751cb26fc79ac179489f416dbd
Bojabija <81344356+Bojabija@users.noreply.github.com>
2021-04-04 19:05:32 +0800

    Update afk.py

--- MashaRoBot/modules/afk.py
+++ MashaRoBot/modules/afk.py
@@ -31,14 +31,14 @@ def afk(update: Update, context: CallbackContext):
         reason = args[1]
         if len(reason) > 100:
             reason = reason[:100]
-            notice = "\nYour afk reason was shortened to 100 characters."
+            notice = "\nAlasan AFK hanya boleh 100 karakter."
     else:
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("📴 {} sekarang AFK ‼️{}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -58,14 +58,14 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "✅ {} telah aktif ‼️",
+                "🔥 {} sudah kembali ‼️",
+                "✅ {} sekarang berada dalam percakapan ‼️",
+                "🔥 {} muncul kembali ‼️",
+                "📱 {} kembali aktif ‼️",
+                "🔥 {} akhirnya muncul lagi‼️",
+                "Selamat datang kembali {} 😎",
+                "Ada yang rindu {}⁉️\nDia telah disini‼️",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
@@ -129,10 +129,10 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         if int(userc_id) == int(user_id):
             return
         if not user.reason:
-            res = "{} is afk".format(fst_name)
+            res = "🚫 {} sedang AFK".format(fst_name)
             update.effective_message.reply_text(res)
         else:
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "❎ {} <i>saat ini AFK.</i>\n\n<b>Karena sedang</b> : <i>{}</i>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
@@ -151,7 +151,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK"
+__mod_name__ = "Afk"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
